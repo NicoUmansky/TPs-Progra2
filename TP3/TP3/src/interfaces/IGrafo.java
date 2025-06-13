9e8e8ac87957381f1c5e959426e07cf4efbce75e
Franco Marcelo Lan <frenchies1111@gmail.com>
2025-06-13 09:31:01 -0300

    Correcion de interfaces

--- TP3/TP3/src/interfaces/IGrafo.java
+++ TP3/TP3/src/interfaces/IGrafo.java
@@ -2,12 +2,57 @@ package interfaces;
 
 import java.util.List;
 
+/**
+ * Interfaz para representar un grafo genérico.
+ */
 public interface IGrafo<T> {
+
+    /**
+     * Pre: dato ≠ null.
+     * Post: Se agrega un nuevo nodo con el dato proporcionado al grafo.
+     * Axioma: getNodos().contains(nodo) ⇐⇒ nodo.dato == dato después de agregar.
+     */
     void agregarNodo(T dato);
+
+    /**
+     * Pre: 0 ≤ origen < getNodos().size() ∧ 0 ≤ destino < getNodos().size().
+     * Post: Se crea una arista desde el nodo en posición 'origen' al nodo en posición 'destino'.
+     * Axioma: mostrarMatrizAdyacencia() muestra un 1 (o valor positivo) en [origen][destino].
+     */
     void agregarArista(int origen, int destino);
+
+    /**
+     * Pre: 0 ≤ inicio < getNodos().size().
+     * Post: Se recorre el grafo en profundidad (DFS) comenzando desde el nodo en 'inicio'.
+     * Axioma: Todos los nodos alcanzables desde 'inicio' son visitados una única vez.
+     */
     void dfs(int inicio);
+
+    /**
+     * Pre: 0 ≤ inicio < getNodos().size().
+     * Post: Se recorre el grafo en anchura (BFS) comenzando desde el nodo en 'inicio'.
+     * Axioma: Todos los nodos alcanzables desde 'inicio' son visitados por niveles (mínima distancia).
+     */
     void bfs(int inicio);
+
+    /**
+     * Pre: - (no tiene precondiciones).
+     * Post: Se imprime la matriz de adyacencia del grafo.
+     * Axioma: matriz[i][j] ≠ 0 ⇐⇒ existe una arista desde el nodo i al nodo j.
+     */
     void mostrarMatrizAdyacencia();
+
+    /**
+     * Pre: - (no tiene precondiciones).
+     * Post: Se devuelve true si el grafo es dirigido, false en caso contrario.
+     * Axioma: esDirigido() ⇐⇒ ∃ i,j : matriz[i][j] ≠ matriz[j][i].
+     */
     boolean esDirigido();
+
+    /**
+     * Pre: - (no tiene precondiciones).
+     * Post: Devuelve la lista de nodos actuales del grafo.
+     * Axioma: getNodos().size() == número de llamadas válidas a agregarNodo().
+     */
     List<INodoGrafo<T>> getNodos();
 }
--- TP3/TP3/src/interfaces/INodoGrafo.java
+++ TP3/TP3/src/interfaces/INodoGrafo.java
@@ -1,8 +1,35 @@
 package interfaces;
 
+/**
+ * Interfaz que representa un nodo dentro de un grafo.
+ */
 public interface INodoGrafo<T> {
+
+    /**
+     * Pre: - (no tiene precondiciones).
+     * Post: Devuelve el dato almacenado en el nodo.
+     * Axioma: Si setDato(x) fue llamado previamente, entonces getDato() == x.
+     */
     T getDato();
+
+    /**
+     * Pre: dato ≠ null.
+     * Post: El dato almacenado en el nodo se actualiza al valor de 'dato'.
+     * Axioma: Después de setDato(x), getDato() == x.
+     */
     void setDato(T dato);
+
+    /**
+     * Pre: - (no tiene precondiciones).
+     * Post: Devuelve el índice asociado al nodo.
+     * Axioma: Si setIndice(i) fue llamado previamente, entonces getIndice() == i.
+     */
     int getIndice();
+
+    /**
+     * Pre: indice ≥ 0.
+     * Post: El índice del nodo se actualiza al valor de 'indice'.
+     * Axioma: Después de setIndice(i), getIndice() == i.
+     */
     void setIndice(int indice);
 }
